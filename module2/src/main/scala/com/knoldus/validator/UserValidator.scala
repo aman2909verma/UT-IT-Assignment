d5ae4c691d4948a844125fcacb05de9e58410e9b
AMAN VERMA <76837547+aman2909verma@users.noreply.github.com>
2021-01-31 23:03:47 +0530

    final commit

--- module2/src/main/scala/com/knoldus/validator/UserValidator.scala
+++ module2/src/main/scala/com/knoldus/validator/UserValidator.scala
@@ -6,15 +6,15 @@ import com.knoldus.models.{Company, User}
 
 class UserValidator {
   def isValid(user: User): Boolean = {
-    // company should exist in db
+    
     val companyReadDto = new CompanyReadDto()
     val dbResult: Option[Company] = companyReadDto.getCompanyByName(user.companyName)
 
-    // email should be valid
+    
     val emailValidator = new EmailValidator()
     val emailValidatorResult = emailValidator.emailIdIsValid(user.emailId)
 
-    if (dbResult.isDefined && emailValidatorResult) return true
+    if (dbResult.isDefined && emailValidatorResult) true
 
     false
   }
