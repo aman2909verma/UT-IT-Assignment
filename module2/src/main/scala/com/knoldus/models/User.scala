60d5b591b5a1913d84db98075605a642d1287577
AMAN VERMA <76837547+aman2909verma@users.noreply.github.com>
2021-01-31 22:38:33 +0530

    Fifth Commit

--- module2/src/main/scala/com/knoldus/models/User.scala
+++ module2/src/main/scala/com/knoldus/models/User.scala
@@ -3,4 +3,5 @@ package com.knoldus.models
 case class User(firstName: String,
                 lastName: String,
                 companyName: String,
+                EmailId: String,
                )
